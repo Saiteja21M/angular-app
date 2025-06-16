1890d5d2bad545f1c8488e9c5a816170f985e579
Sai Teja Koppala <koppalasaiteja21@gmail.com>
2025-06-16 20:29:09 +0530

    fix: Update host URL in app component to point to the correct server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,7 @@ import { Show } from './show.model';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  host = 'http://3.79.204.213:8080/';
+  host = 'http://172.31.4.118:8080/';
   title = 'angular-app';
   searchQuery: string = '';
   shows: Show[] = [];
