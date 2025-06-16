d3bb240b9d6f622ba013425e59dcfdc87d1bc407
saiteja <saiteja.koppala@nttadata.com>
2025-06-16 19:41:19 +0530

    fix: Update host URL in app component to point to the correct API endpoint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,7 @@ import { Show } from './show.model';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  host = 'http://localhost:8082';
+  host = 'http://http://3.79.204.213:8080/';
   title = 'angular-app';
   searchQuery: string = '';
   shows: Show[] = [];
